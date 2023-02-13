0f4167c66dd8074679c208cd0901e04f748ec203
AndreyRomanuk <krark1986@github.com>
2023-02-13 18:43:45 +0300

    Create Control Homework project

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿string[] strings = InputStringArray();
+int numberStrings = InputInt("Введите максимальную длину строки: ");
+string[] resultArray = FindStringByLength(string, numberStrings);
+
+PrintStringArray(strings, "Исходный массив");
+PrintStringArray(resultArray, "Соответствующие критерию строки");
+
+string[] FindStringByLength(string[] stringArray, int limit)
+{
+    int size = 0;
+    string[] resultArray = new string[size];
+
+    for (int i = 0, i < stringArray.Length, i++)
+    {
+        if (stringArray[i].Length <= limit)
+        {
+            size++;
+            Array.Resize(ref resultArray, size);
+            resultArray[size-1] = stringArray[i];
+        }
+    }
+    return resultArray;
+}
+
+void PrintStringArray(string[] array, string text)
+{
+    string str = string.Join("; ", array);
+    Console.WriteLine($"{text}: [{str}]");
+}
+
+string[] InputStringArray()
+{
+    int length = InputInt("Введите количество элеменктов: ");
+    string[] array = new string[length];
+
+    for (int i = 0, i < array.Length, i++)
+    {
+        array[i] = InputString($"Введите элемент №{i}: ")
+    }
+    return array;
+}
+
+string InputString(string text)
+{
+    Console.WriteLine(text);
+    string str = Console.ReadLine();
+    return str;
+}
+
+int Input(string text)
+{
+    bool isParsed = false;
+    int number = 0;
+
+    while (!isParsed)
+    {
+        Console.WriteLine(text);
+        isParsed = int.TryParse(Console.ReadLine(), out number);
+    }
+    return number;
+}
